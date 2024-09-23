19f54ce1f2b55f23da620887b8ccd55eaaef14be
Smoreno101 <98426245+10moreno14@users.noreply.github.com>
2024-09-23 13:12:23 -0500

    Create generateHash.js

---
+++ generateHash.js
@@ -0,0 +1,13 @@
+onst bcrypt = require('bcrypt');
+
+const password = 'Octubre14.'; // Cambia esto por la contraseña que deseas hashear
+const saltRounds = 10;
+
+bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+        console.error('Error al generar el hash:', err);
+    } else {
+        console.log('Hash generado:', hash);
+        // Aquí puedes copiar el hash para almacenarlo en tu base de datos
+    }
+});
